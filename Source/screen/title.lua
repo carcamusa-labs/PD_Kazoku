aed9f655bcd5300c669c8970c9f9b492bf7f1abb
Nitram <mani-designs@hotmail.com>
2024-12-07 18:58:52 -0600

    repo init

---
+++ Source/screen/title.lua
@@ -0,0 +1,30 @@
+TITLE_SCREEN = 'TitleScreen'
+
+class(TITLE_SCREEN).extends(Screen)
+
+
+local pd = playdate
+local gfx = pd.graphics
+
+
+-- Screen lifecycle
+
+function TitleScreen:init()
+    TitleScreen.super.init(self)
+end
+
+function TitleScreen:onAppear()
+    -- drawBgImg('characters', 'the_father')
+    -- gfx.fillRect(100, 100, 100, 20)
+
+    playdate.graphics.drawText("Hello World Again", 200, 120)
+end
+
+function TitleScreen:onDisappear()
+end
+
+function TitleScreen:update()
+    if playdate.buttonJustReleased('a') or playdate.buttonJustReleased('b') then
+        global.onChangeScreen(EXPLORE_SCREEN, 1)
+    end
+end
